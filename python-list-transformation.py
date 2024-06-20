27cedc9d67a100930e7cb2e5aceade2746b66511
taylornich <taylormanhard15@gmail.com>
2024-06-20 15:31:00 -0700

    python list assignment question 1

---
+++ python-list-transformation.py
@@ -0,0 +1,16 @@
+# question 1 task 1
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+
+descending_grades = grades
+
+descending_grades.sort(reverse=True)
+
+print("Grades in descending order: ", descending_grades)
+
+
+# question 1 task 2
+
+average_grade = sum(grades) / len(grades)
+
+print("Average grade: ", average_grade)
