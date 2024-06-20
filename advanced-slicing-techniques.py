ec486ee33f6e53ecf0e2e85969622edf146617ea
taylornich <taylormanhard15@gmail.com>
2024-06-20 15:39:40 -0700

    Python Lists Assignment Question 3

---
+++ advanced-slicing-techniques.py
@@ -0,0 +1,12 @@
+temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 105, 106]
+
+# question 3 task 1
+week_two_temps = temperatures[7:14]
+
+print(week_two_temps)
+
+# question 3 task 2
+
+above_100 = temperatures[15:]
+
+print(above_100)
